ee6c3de618f8423b65b73a4cc354c46285b025c3
evertdf <edefeij@live.nl>
2021-01-04 16:34:19 +0100

    added limit option to findall and added new interface method count

--- components/core/adapter/mapper/Base.php
+++ components/core/adapter/mapper/Base.php
@@ -1,9 +1,12 @@
 <?php
 namespace Components\Core\Adapter\Mapper {
     interface Base {                
+        
+        public function count(array $validations = [], string $query = NULL) : int;
+        
         public function find(array $validations = [], string $query = NULL);
         
-        public function findAll(array $validations = [], string $query = NULL) : array;
+        public function findAll(array $validations = [], int $position = 0, int $limit = 0, string $query = NULL) : array;
         
         public function save();
         
