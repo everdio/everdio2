226b158913da95d7319f34862cf6c16aad94bcdf
evertdf <edefeij@live.nl>
2021-01-08 10:39:33 +0100

    created easy functions for adapter from static

--- components/Path.php
+++ components/Path.php
@@ -2,11 +2,11 @@
 namespace Components {
     use FilesystemIterator, RecursiveIteratorIterator;
     class Path extends \RecursiveIteratorIterator {
-        public function __construct($path, $mode = 0776, $group = "www-data") {
+        public function __construct($path, $create = true, $mode = 0776, $group = "www-data") {
             try {
                 parent::__construct(new \RecursiveDirectoryIterator($path, FilesystemIterator::SKIP_DOTS), RecursiveIteratorIterator::CHILD_FIRST);
             } catch (\Exception $ex) {
-                if (mkdir($path, $mode, true)) {  
+                if ($create && mkdir($path, $mode, true)) {  
                    chgrp($path, $group);                   
                    return self::__construct($path, $mode, $group);
                 }
--- components/core/Adapter.php
+++ components/core/Adapter.php
@@ -1,9 +1,29 @@
 <?php
 namespace Components\Core {
     abstract class Adapter extends \Components\Core {
-        static protected $instances = [];
+        static private $_adapters = [];
         
         abstract protected function initialise();
+        
+        protected function hadAdapter($key) {
+            return (bool) array_key_exists($key, self::$_adapters);
+        }
+        
+        protected function getAdapter($key) {
+            if ($this->hadAdapter($key)) {
+                return self::$_adapters[$key];
+            }
+            
+            throw new RuntimeException(sprintf("unknown or invalid instance by key %s", $key));
+        }
+        
+        protected function addAdapter($key, $instance, $overwrite = true) {
+            if ($overwrite || !$overwrite && $this->hasAdapter($key)) {
+                return (object) self::$_adapters[$key] = $instance;
+            }
+            
+            throw new RuntimeException(sprintf("found existing adapter by key %s", $key));
+        }
 
         final public function uses(string $trait) : bool {
             return (bool) in_array($trait, class_uses($this));
