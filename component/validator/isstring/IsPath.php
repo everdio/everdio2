27fd3fa630b4c431b39172c15e1374e09a97becd
evertdf <edefeij@live.nl>
2022-05-09 12:06:41 +0200

    paths are validated true with _ in them

--- component/validator/isstring/IsPath.php
+++ component/validator/isstring/IsPath.php
@@ -4,7 +4,7 @@ namespace Component\Validator\IsString {
         const TYPE = "IS_PATH";
         const MESSAGE = "INVALID_PATH";
         public function execute($value) : bool {       
-            return (bool) (parent::execute($value) && $value === \preg_replace("/[^A-Za-z0-9\/]/", false, $value));
+            return (bool) (parent::execute($value) && $value === \preg_replace("/[^_A-Za-z0-9\/]/", false, $value));
             //return (bool) (parent::execute($value) && (\strlen($value) === \strlen(\strip_tags($value)) && \sizeof(\explode(\DIRECTORY_SEPARATOR, \strip_tags($value)))));
         }
     }
