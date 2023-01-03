f00593f159d087b7cef04346ef2bc53954554c21
evertdf <edefeij@live.nl>
2023-01-03 13:33:32 +0100

    added cli codes

--- component/core/controller/model/Cli.php
+++ component/core/controller/model/Cli.php
@@ -7,12 +7,12 @@ namespace Component\Core\Controller\Model {
                 "server" => new Validation(false, [new Validator\IsArray\Intersect\Key(["argv", "argc"])]),
                 "request" => new Validation(new \Component\Core\Parameters, [new Validator\IsObject\Of("\Component\Core\Parameters")]),
                 "route" => new Validation(false, [new Validator\IsString\IsPath]),
-                "codes" => new Validation(["bold" => 1, "italic" => 3, "underline" => 4, "strikethrough" => 9, "black" => 30, "red" => 31, "green" => 32, "yellow" => 33, "blue" => 34, "magenta" => 35, "cyan" => 36, "white" => 37, "blackbg" => 40, "redbg" => 41, "greenbg" => 42, "yellowbg" => 44,"bluebg" => 44, "magentabg" => 45, "cyanbg" => 46, "lightgreybg" => 47], [new Validator\IsArray])
+                "styles" => new Validation(["bright" => 1, "italic" => 3, "underline" => 4, "blinking" => 5, "strikethrough" => 9, "black" => 30, "red" => 31, "green" => 32, "yellow" => 33, "blue" => 34, "magenta" => 35, "cyan" => 36, "white" => 37, "blackbg" => 40, "redbg" => 41, "greenbg" => 42, "yellowbg" => 44,"bluebg" => 44, "magentabg" => 45, "cyanbg" => 46, "lightgreybg" => 47], [new Validator\IsArray])
             ] + $_parameters);
         }
         
         final public function echo(string $text, array $format = ["white", "blackbg"]) {
-            return (string) \sprintf("\033[0m\033[%sm%s\033[0m", \implode(";", \array_flip(\array_intersect(\array_flip($this->codes), $format))), $text);
+            return (string) \sprintf("\033[%sm%s\033[0m", \implode(";", \array_flip(\array_intersect(\array_flip($this->styles), $format))), $text);
         }
         
         final public function setup(array $request = [], array $arguments = []) : void {
