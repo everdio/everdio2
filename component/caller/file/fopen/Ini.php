025448a410db79a86b4b4532aa2374636be07947
evertdf <edefeij@live.nl>
2023-02-26 10:56:44 +0100

    set default openai model, improved fopen/ini

--- component/caller/file/fopen/Ini.php
+++ component/caller/file/fopen/Ini.php
@@ -5,24 +5,30 @@ namespace Component\Caller\File\Fopen {
             parent::__construct(\sprintf("%s.ini", $path), "c");
         }
         
-        public function writeSection(string $section) {
-            $this->write(\sprintf("[%s]\n", $section));
+        final public function writeSection(string $section) {
+            if (!empty($section)) {
+                $this->write(\sprintf("[%s]\n", $section));
+            }
         }
         
-        public function writeValue($value) : string {
+        private function _value($value) : string {
             return (string) \addcslashes(\trim(\trim($this->dehydrate($value), "'")), "\"");
         }
         
-        public function writePair(string $parameter, $value) {
-            $this->write(\sprintf("%s = \"%s\";\n", $parameter, $this->writeValue($value)));
+        final public function writePair(string $parameter, $value) {
+            if (!empty($parameter)) {
+                $this->write(\sprintf("%s = \"%s\";\n", $parameter, $this->_value($value)));
+            }
         }
 
-        public function writeKeyPair(string $key, string $parameter, $value) {
-            $this->write(\sprintf("%s[%s] = \"%s\";\n", $key, $parameter, $this->writeValue($value)));
+        final public function writeKeyPair(string $key, string $parameter, $value) {
+            if (!empty($key) && !empty($parameter)) {
+                $this->write(\sprintf("%s[%s] = \"%s\";\n", $key, $parameter, $this->_value($value)));
+            }
         }
 
-        public function writeArray(array $array) {
-            foreach ($array as $key1 => $value1) {
+        final public function writeArray(array $array1) {
+            foreach ($array1 as $key1 => $value1) {
                 if (\is_array($value1)) {
                     foreach ($value1 as $key2 => $value2) {
                         $this->writeKeyPair($key1, $key2, $value2);
@@ -31,6 +37,11 @@ namespace Component\Caller\File\Fopen {
                     $this->writePair($key1, $value1);
                 }
             }
+        }
+        
+        final public function store(string $section, array $array) {
+            $this->writeSection($section);
+            $this->writeArray($array);
         }
     }
 }
