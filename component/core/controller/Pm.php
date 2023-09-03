1a5f45a0e3cb2c93657ebf4afe2b659e0b66b0ee
evertdf <edefeij@live.nl>
2023-09-03 20:37:14 +0200

    process manager

--- component/core/controller/Pm.php
+++ component/core/controller/Pm.php
@@ -5,9 +5,7 @@ namespace Component\Core\Controller {
     final class Pm extends \Component\Core\Controller {
 
         public function execute(string $path): void {
-            if (isset($this->oid) && $this->oid === "linux") {
-                \exec(\sprintf("%s %s %s > /dev/null &", $path, (isset($this->arguments) ? \sprintf("--%s", \implode(" --", \explode(\DIRECTORY_SEPARATOR, $this->arguments))) : false), \http_build_query($this->request->restore(), false, " ")));
-            }
+            \exec(\sprintf("%s %s %s  > /dev/null &", $path, (isset($this->arguments) ? \sprintf("--%s", $this->arguments) : false), \http_build_query($this->request->restore(), false, " ")));
         }
     }
 
