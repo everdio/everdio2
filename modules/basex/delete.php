ae30c79435b0c908afdfe5d09907913c295d9aed
evertdf <edefeij@live.nl>
2024-04-15 14:37:18 +0200

    basic actins basex

--- modules/basex/delete.php
+++ modules/basex/delete.php
@@ -1,15 +1,11 @@
 <?php
+
 if ($this instanceof Component\Core\Controller\Model\Cli) {
-    $this->echo("BaseX Curl " . $this->basex->host . \DIRECTORY_SEPARATOR . $this->request->database, ["bold", "white"]);
-    $this->echo(" deleting ", ["bold", "green"]);
-    $this->echo(\sprintf("%s .. ", $this->request->resource), ["bold", "yellow"]);
-    
     $curl = new \Component\Caller\Curl;
     $curl->setopt_array([
         \CURLOPT_HTTPAUTH => \CURLAUTH_BASIC,
         \CURLOPT_USERPWD => $this->basex->username . ":" . $this->basex->password,
-        \CURLOPT_URL => $this->basex->host . \DIRECTORY_SEPARATOR . $this->request->database . \DIRECTORY_SEPARATOR . $this->request->resource]);    
+        \CURLOPT_URL => $this->basex->host . \DIRECTORY_SEPARATOR . $this->request->database . \DIRECTORY_SEPARATOR . $this->request->resource]);
     $curl->delete();
-    $this->echo($curl->execute(), ["white", "bold"]);
-    $this->break(1);            
+    $curl->execute();
 }
--- modules/basex/post.php
+++ modules/basex/post.php
@@ -1,9 +1,5 @@
 <?php
 if ($this instanceof Component\Core\Controller\Model\Cli) {
-    $this->echo("BaseX Curl " . $this->request->basex->host . \DIRECTORY_SEPARATOR . $this->request->basex->database, ["bold", "white"]);
-    $this->echo(" posting ", ["bold", "green"]);
-    $this->echo(\sprintf("%s .. ", $this->request->basex->command), ["bold", "yellow"]);
-
     $file = new \Component\Caller\File\Fopen(__DIR__ . \DIRECTORY_SEPARATOR . $this->request->basex->command, "r");
 
     $curl = new \Component\Caller\Curl;
@@ -13,6 +9,4 @@ if ($this instanceof Component\Core\Controller\Model\Cli) {
         \CURLOPT_URL => $this->request->basex->host]);            
     $curl->post($this->request->basex->replace($file->read(\filesize($file->file))));
     $curl->execute();
-    $this->echo("done", ["green", "bold"]);
-    $this->break();
 }
