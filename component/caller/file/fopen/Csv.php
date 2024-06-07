44c8da8459bb8e6757f358ab7ac694dba82f706e
evertdf <edefeij@live.nl>
2024-06-07 12:00:28 +0200

    csv change

--- component/caller/file/fopen/Csv.php
+++ component/caller/file/fopen/Csv.php
@@ -3,7 +3,10 @@
 namespace Component\Caller\File\Fopen {
 
     class Csv extends \Component\Caller\File\Fopen {
-
+        public function __construct(string $path, string $mode = "r") {
+            parent::__construct(\sprintf("%s.csv", $path), $mode);
+        }
+        
         public function read(array $keys, int $length = 0, string $separator = ",", string $enclosure = "\"", string $escape = "\\", array $rows = []): array {
             while (!$this->eof() && ($data = $this->getcsv($length, $separator, $enclosure, $escape))) {
                 $rows[] = \array_combine($keys, $data);
